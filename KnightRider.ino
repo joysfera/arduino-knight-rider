c3c02bbb20247877336fd092ce39cf1525d06c4f
Petr Stehlík <petr@pstehlik.cz>
2016-01-05 22:38:43 +0100

    LED pins are configurable direct register access is configurable LED to VCC or GND is configurable several new effects added

--- KnightRider.ino
+++ KnightRider.ino
@@ -11,20 +11,34 @@
 #include <TimerOne.h>
 
 #define PWM_PINS    8
+#define START_PIN   2
+
+#define FAST_WRITE  true
+#define LED_TO_PLUS true
 
 volatile bool leds[PWM_PINS];
 volatile byte pwm_regs[PWM_PINS];
 volatile byte kittMode;
+volatile byte kittIndex;
 
 void setup()
 {
-    for(byte i=2; i<10; i++) {
+    for(byte i = START_PIN; i < START_PIN+PWM_PINS; i++) {
         pinMode(i, OUTPUT);
     }
     LEDoff();
+
     setKittMode(2);
+
     Timer1.initialize(64);  // 15625 Hz => 8bit PWM 61 Hz refresh rate
     Timer1.attachInterrupt(myIrq);
+}
+
+void LEDoff()
+{
+    for(byte i = START_PIN; i < START_PIN+PWM_PINS; i++) {
+        digitalWrite(i, LED_TO_PLUS ? HIGH : LOW);
+    }
 }
 
 void loop()
@@ -33,38 +47,49 @@ void loop()
     delay(72);  // this is the exact speed of the original Knight Rider intro (on the desert)
 }
 
-void LEDoff()
-{
-    for(byte i=2; i<10; i++) {
-        digitalWrite(i, HIGH);
-    }
-}
-
 void setKittMode(byte mode)
 {
     kittMode = -1;
+    kittIndex = 0;
     LEDoff();
     kittMode = mode;
 }
 
 void nextKittStep()
 {
-    static byte index;
     static bool dirRight;
 
     switch(kittMode) {
-        case 0: leds[index] = LOW;
-                if (++index >= PWM_PINS) index = 0;
-                leds[index] = HIGH;
+        case 0: leds[kittIndex] = LOW;
+                if (++kittIndex >= PWM_PINS) kittIndex = 0;
+                leds[kittIndex] = HIGH;
                 break;
-        case 1: leds[index] = LOW;
-                if (--index >= PWM_PINS) index = PWM_PINS-1;
-                leds[index] = HIGH;
+
+        case 1: leds[kittIndex] = LOW;
+                if (--kittIndex >= PWM_PINS) kittIndex = PWM_PINS-1;
+                leds[kittIndex] = HIGH;
                 break;
-        case 2: leds[index] = LOW;
-                if (dirRight && ++index >= PWM_PINS) { index = PWM_PINS-2; dirRight = false; }
-                else if (!dirRight && --index >= PWM_PINS) { index = 1; dirRight = true; }
-                leds[index] = HIGH;
+
+        case 2: leds[kittIndex] = LOW;
+                if (dirRight && ++kittIndex >= PWM_PINS) { kittIndex = PWM_PINS-2; dirRight = false; }
+                else if (!dirRight && --kittIndex >= PWM_PINS) { kittIndex = 1; dirRight = true; }
+                leds[kittIndex] = HIGH;
+                break;
+
+        case 3: leds[kittIndex] = leds[PWM_PINS-1 - kittIndex] = LOW;
+                if (++kittIndex >= PWM_PINS/2) kittIndex = 0;
+                leds[kittIndex] = leds[PWM_PINS-1 - kittIndex] = HIGH;
+                break;
+
+        case 4: leds[kittIndex] = leds[PWM_PINS-1 - kittIndex] = LOW;
+                if (--kittIndex >= PWM_PINS) kittIndex = PWM_PINS/2-1;
+                leds[kittIndex] = leds[PWM_PINS-1 - kittIndex] = HIGH;
+                break;
+
+        case 5: leds[kittIndex] = leds[PWM_PINS-1 - kittIndex] = LOW;
+                if (dirRight && ++kittIndex >= PWM_PINS/2) { kittIndex = PWM_PINS/2-2; dirRight = false; }
+                else if (!dirRight && --kittIndex >= PWM_PINS) { kittIndex = 1; dirRight = true; }
+                leds[kittIndex] = leds[PWM_PINS-1 - kittIndex] = HIGH;
                 break;
     }
 }
@@ -89,24 +114,37 @@ void softPWM(void)
         counter++;
     }
 
-    byte state = 0;   
+#if FAST_WRITE
+    byte state = 0;
+#endif
     for(byte i = 0; i < PWM_PINS; i++) {
         bool b = false;
         if (shadows[i]) {
             shadows[i]--;
             b = true;
         }
+#if FAST_WRITE
         state = state << 1 | b;
+#else
+        digitalWrite(START_PIN + i, LED_TO_PLUS ? !b : b);
+#endif
     }
+#if FAST_WRITE
+# if LED_TO_PLUS
     PORTD = (PORTD & 0x03) | ~(state << 2);
     PORTB = (PORTB & ~0x03) | ~(state >> 6);
+# else
+    PORTD = (PORTD & 0x03) | (state << 2);
+    PORTB = (PORTB & ~0x03) | (state >> 6);
+# endif
+#endif
 }
 
 void fadeOutEffect(void)
 {
     for(byte i = 0; i < PWM_PINS; i++) {
         if (leds[i] && pwm_regs[i] != 255) {
-            unsigned x = pwm_regs[i] + 32;  // quickly light up (in 4 steps)
+            unsigned x = pwm_regs[i] + 64;  // quickly light up (in 4 steps)
             if (x > 255) x = 255;
             pwm_regs[i] = x;
         }
