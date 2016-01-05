1558167f270cd4856e3c7bb6c19154d63a42f229
Petr Stehlík <petr@pstehlik.cz>
2016-01-05 21:20:13 +0100

    more effects, out of the main loop into a state machine

--- KnightRider.ino
+++ KnightRider.ino
@@ -2,6 +2,9 @@
  * Knight Rider (actually K.I.T.T.) 8xLED scanner bar
  * 
  * Petr Stehlik in December 2015
+ *
+ * petr@pstehlik.cz
+ *
  * licensed under the GPLv3
  */
 
@@ -11,38 +14,59 @@
 
 volatile bool leds[PWM_PINS];
 volatile byte pwm_regs[PWM_PINS];
+volatile byte kittMode;
 
 void setup()
 {
     for(byte i=2; i<10; i++) {
         pinMode(i, OUTPUT);
-        digitalWrite(i, HIGH); // LED OFF
     }
+    LEDoff();
+    setKittMode(2);
     Timer1.initialize(64);  // 15625 Hz => 8bit PWM 61 Hz refresh rate
     Timer1.attachInterrupt(myIrq);
 }
 
 void loop()
 {
-    static byte x;
-    static bool dir = false;
-
-    for(byte i = 0; i < PWM_PINS; i++) {
-        leds[i] = (i == x);
-    }
-    if (dir) {
-        if (++x == PWM_PINS) {
-            dir = !dir;
-            x = PWM_PINS-2;
-        }
-    }
-    else {
-        if (!x--) {
-            dir = !dir;
-            x = 1;
-        }
-    }
+    nextKittStep();
     delay(72);  // this is the exact speed of the original Knight Rider intro (on the desert)
+}
+
+void LEDoff()
+{
+    for(byte i=2; i<10; i++) {
+        digitalWrite(i, HIGH);
+    }
+}
+
+void setKittMode(byte mode)
+{
+    kittMode = -1;
+    LEDoff();
+    kittMode = mode;
+}
+
+void nextKittStep()
+{
+    static byte index;
+    static bool dirRight;
+
+    switch(kittMode) {
+        case 0: leds[index] = LOW;
+                if (++index >= PWM_PINS) index = 0;
+                leds[index] = HIGH;
+                break;
+        case 1: leds[index] = LOW;
+                if (--index >= PWM_PINS) index = PWM_PINS-1;
+                leds[index] = HIGH;
+                break;
+        case 2: leds[index] = LOW;
+                if (dirRight && ++index >= PWM_PINS) { index = PWM_PINS-2; dirRight = false; }
+                else if (!dirRight && --index >= PWM_PINS) { index = 1; dirRight = true; }
+                leds[index] = HIGH;
+                break;
+    }
 }
 
 void myIrq(void)
